aff94fce088f2c3fb502f7fcf34aa7bdfc6e9d9f
Jose Luis <josepaternina@gmail.com>
2021-10-09 00:29:09 +0200

    01.Qt-Window, simple window,  main README updated

---
+++ 01.Window/01.Qt-window.py
@@ -0,0 +1,13 @@
+# Create a simple window with Python and Qt5 
+import sys
+from PyQt5.QtWidgets import QMainWindow as QMainW, QApplication as QApp
+
+class MainWindow(QMainW):
+    def __init__(self, *args, **kwargs):
+      super(MainWindow, self).__init__(*args, **kwargs)
+      self.show()
+
+app = QApp(sys.argv)
+window = MainWindow()
+
+app.exec_()
